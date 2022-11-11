92cff2c150b20a758f7b76291eb597d2259399f2
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2022-11-11 10:24:37 +0700

    variable datatype

---
+++ Module_1/Variable_Datatype/variable.js
@@ -0,0 +1,35 @@
+// var number = 18;
+// console.log(typeof number);
+// number = "Khoa";
+// console.log(typeof number);
+// number = true;
+// console.log(typeof number);
+// number = undefined;
+// console.log(typeof number);
+// number = null;
+// console.log(typeof number);
+// number = {};
+// console.log(typeof number);
+// number = function(){};
+// console.log(typeof number);
+
+// var n = 5;
+// console.log(n ++ > 5);
+// console.log(++n > 5);
+// console.log(n);
+
+// console.log('5' == 5);
+// console.log('5' === 5);
+
+// var age = 19;
+// var gender = false;
+
+// console.log(age == 18 || gender == true);
+
+console.log(typeof(5 + 7 ));
+console.log(typeof (5 + 7 + '3' ));
+console.log(5 + 7 + '3' + 2 );
+console.log(5 + 7 + '3' + 2 + 3);
+
+// template string || string iliteral
+
