a058c519c5ce1e3344982a500e92d45c1101a980
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2022-11-23 11:36:40 +0700

    function

---
+++ Module_1/Function/function.js
@@ -0,0 +1,17 @@
+let number_1 = 10;
+let number_2 = 20;
+
+function sum(para_1, para_2){
+    // para_1 += 2;
+    // para_2 += 2;
+    console.log(`1. ${para_1}, ${para_2}`);
+}
+sum(5, 15);
+
+// console.log(`1. ${number_1}, ${number_2}`); //10, 20
+// sum(number_1, number_2); //12,22
+// console.log(`1. ${number_1}, ${number_2}`); // 10,20 | 12, 22
+
+
+// viết hàm generateArray, có size = 10 và các giá trị được sinh ngẫu nhiên trong [10, 50]
+// và hiển thị mảng này ra console
