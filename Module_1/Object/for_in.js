cd048c1ef4945ed47c45775e0217363608b25276
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2022-11-28 10:20:01 +0700

    update

--- Module_1/Object/for_in.js
+++ Module_1/Object/for_in.js
@@ -1,12 +1,13 @@
 let numbers = [1, 5, 4, 5, 5, 6, 8, 9, 9];
+numbers.length = 12;
 // for(let i = 0; i < numbers.length; i++){
 //     console.log(numbers[i]);
 // }
 
-for(let index in numbers){
-    // console.log(numbers[index]);
-    console.log(`${index} - ${numbers[index]}`);
-}
+// for(let index in numbers){
+//     // console.log(numbers[index]);
+//     console.log(`${index} - ${numbers[index]}`);
+// }
 
 
 // for(let number of numbers){
